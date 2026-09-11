590c7ab8fec0a70563d2090c57a2de0d92e75f77
lifesyo <lifesyo4030@gmail.com>
2026-09-11 22:29:39 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+/* Note IQ Service Worker */
+const VERSION = 'noteiq-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(VERSION).then(c => c.addAll(ASSETS)).catch(()=>{}));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== VERSION).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  // GET以外、または外部ドメイン（Firebase/Google/CDN等）はキャッシュせず素通し
+  if (req.method !== 'GET' || url.origin !== self.location.origin) {
+    return; // ブラウザ標準の取得に任せる
+  }
+
+  // 自サイトの静的ファイル：ネット優先、失敗したらキャッシュ
+  e.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(VERSION).then(c => c.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match(req).then(r => r || caches.match('./index.html')))
+  );
+});
